9e770f191e423b0a86cd07d548c89a425c60dfce
Monica-Araujo <monicaaraujo974@gmail.com>
2021-06-13 16:23:50 -0300

    Header e footer ainda em estilização, não estão prontos

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,25 +4,28 @@ import styled from "styled-components"
 const Container = styled.div`
 display: flex;
 justify-content: center;
+align-items: center;
 width:100%;
-height: 100px;
+height: 50px;
 position: relative;
 margin: 0;
+margin-top: 90px;
+background-color: darkgrey;
 `
 
 const Title = styled.h2`
 display: flex;
-font-size: 1.5em;
+font-size: 1em;
 text-align: center;
 justify-content: center;
-color: green;
+color: white;
 `;
 
 class Footer extends React.Component {
   render(){
     return(
       <Container>
-        <Title> Eu sou o footer </Title>
+        <Title>© Todos os direitos reservados</Title>
       </Container>
     )
   }
